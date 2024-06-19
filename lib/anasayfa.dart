18c20b1071b76be08ea990b7c3fd9c6e6e787360
FERAT TAS <ferattas2763@gmail.com>
2024-06-19 21:18:27 +0300

    Düzenlemeler

--- lib/anasayfa.dart
+++ lib/anasayfa.dart
@@ -13,16 +13,96 @@ class _AnasayfaState extends State<Anasayfa> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.menu, color: anaRenk),
+          onPressed: () {
+            // Menü butonuna tıklandığında yapılacak işlemler
+          },
+        ),
         title: Text(
           "QrFooder",
-          style:
-              TextStyle(color: anaRenk, fontFamily: "LogoFont", fontSize: 29),
+          style: TextStyle(
+            color: anaRenk,
+            fontFamily: "LogoFont",
+            fontSize: 40,
+          ),
         ),
-        backgroundColor:
-            anaRenk1, 
+        backgroundColor: anaRenk1,
         centerTitle: true,
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.person,
+              color: buttonrenk2,
+            ),
+            iconSize: 30.0,
+            onPressed: () {
+              // Kullanıcı butonuna tıklandığında yapılacak işlemler
+            },
+          ),
+        ],
       ),
-      body: const Center(),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 30.0, left: 30.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Hi Alper",
+              style: TextStyle(
+                color: anayazirenk,
+                fontFamily: "AnaFont2",
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              "Looking for something to eat?",
+              style: TextStyle(
+                color: anayazirenk,
+                fontFamily: "AltFont",
+                fontSize: 17,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 80.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Choose Category",
+                    style: TextStyle(
+                      color: anayazirenk,
+                      fontFamily: "AnaFont2",
+                      fontSize: 22.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(right: 25.0), // 10 px sola al
+                    child: InkWell(
+                      onTap: () {
+                        // "See All" yazısına tıklandığında yapılacak işlemler
+                      },
+                      child: Text(
+                        "See All",
+                        style: TextStyle(
+                          color:
+                              yazirenk, // İstediğiniz rengi ayarlayabilirsiniz
+                          fontFamily: "AltFont",
+                          fontSize: 17.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/colors.dart
+++ lib/colors.dart
@@ -5,3 +5,4 @@ var anaRenk = const Color(0xFF0D0D0D);
 var buttonrenk1 = const Color(0xFF17C37B);
 var buttonrenk2 = const Color(0xFFF7B500);
 var yazirenk = const Color(0xFF8A959E);
+var anayazirenk = const Color(0xFF12162D);
