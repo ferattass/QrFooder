c56772336150f01bfffe6a57c5c09b23f8eea86e
FERAT TAS <ferattas2763@gmail.com>
2024-06-20 18:04:23 +0300

    Category icons and necessary edits

--- lib/anasayfa.dart
+++ lib/anasayfa.dart
@@ -100,6 +100,66 @@ class _AnasayfaState extends State<Anasayfa> {
                 ],
               ),
             ),
+            SizedBox(
+                height:
+                    20.0), // "Choose Category" ve butonlar arasına boşluk ekler
+            Expanded(
+              child: GridView.count(
+                crossAxisCount: 2, // 2 sütunlu grid
+                crossAxisSpacing: 20.0, // Sütunlar arasındaki boşluk
+                mainAxisSpacing: 20.0, // Satırlar arasındaki boşluk
+                padding: const EdgeInsets.all(20.0),
+                children: <Widget>[
+                  buildCategoryCard('assets/fish.png', 'Sea Foods', () {
+                    // İkon 1 tıklama işlemleri
+                  }),
+                  buildCategoryCard('assets/pizza.png', 'Pizza', () {
+                    // İkon 2 tıklama işlemleri
+                  }),
+                  buildCategoryCard('assets/vegan.png', 'Vegan', () {
+                    // İkon 3 tıklama işlemleri
+                  }),
+                  buildCategoryCard('assets/chinese.png', 'Chinese Food', () {
+                    // İkon 4 tıklama işlemleri
+                  }),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildCategoryCard(String assetPath, String title, VoidCallback onTap) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        width: 181,
+        height: 132,
+        decoration: BoxDecoration(
+          color: Colors.grey[200], // Dikdörtgenin arka plan rengi
+          borderRadius:
+              BorderRadius.circular(10), // Dikdörtgenin köşe yuvarlaklığı
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              assetPath,
+              width: 60.0, // İkonun genişliği
+              height: 60.0, // İkonun yüksekliği
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              title,
+              style: TextStyle(
+                color: anayazirenk, // İstediğiniz yazı rengi
+                fontFamily: "AltFont",
+                fontSize: 17.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
           ],
         ),
       ),
--- lib/colors.dart
+++ lib/colors.dart
@@ -6,3 +6,4 @@ var buttonrenk1 = const Color(0xFF17C37B);
 var buttonrenk2 = const Color(0xFFF7B500);
 var yazirenk = const Color(0xFF8A959E);
 var anayazirenk = const Color(0xFF12162D);
+var anarenk3 = const Color(0xFF5965523);
